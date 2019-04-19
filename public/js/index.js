b3d0fc05f44ac8575a435d8c900b9154c479dd99
Feras Tajuore <ferastajuore@yahoo.com>
2019-04-19 15:28:53 +0200

    create file js

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+// Making request from clint to server to open a websocket and keep connection open
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+})
+
+socket.on('disconnect', function() {
+    console.log('Disconnect from server');
+})
+
+socket.on('newMessage', function (message) {
+    console.log('New Message', message);
+})
+
