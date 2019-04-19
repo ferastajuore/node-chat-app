479ddf1d159b1829047d6125f4b4ad672e244012
Feras Tajuore <ferastajuore@yahoo.com>
2019-04-19 15:55:43 +0200

    Greet new user, and create others

--- server/server.js
+++ server/server.js
@@ -19,12 +19,31 @@ app.use(express.static(publicPath))
 io.on('connection',(socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage',{
+    from: 'Admin',
+    text: 'Welcome To the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'New user join',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('Create Message', message);
     io.emit('newMessage',{
       from: message.from,
-      text: message.text
+      text: message.text,
+      createdAt: new Date().getTime()
     })
+
+    // socket.broadcast.emit('newMessage',{
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // })
   })
 
   socket.on('disconnect',() => {
